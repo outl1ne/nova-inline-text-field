7d3a00d645520bccb5f3905d82bb8ba3a9ea3649
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2021-10-21 16:18:20 +0300

    UX fixes

--- resources/js/components/IndexField.vue
+++ resources/js/components/IndexField.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     :class="`nova-inline-text-field-index text-${field.textAlign}${editing ? ' -editing' : ''}`"
-    @click.capture="editing = true"
+    @click="startEditing"
   >
     <template v-if="hasValue">
       <template v-if="!editing">
@@ -19,6 +19,7 @@
           v-model="fieldValue"
           @keypress="onKeyPress"
           type="text"
+          :disabled="loading"
           class="form-control form-input form-input-bordered"
           autofocus
         />
@@ -27,7 +28,7 @@
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
           class="inline-icon confirm-icon"
-          @click.capture="updateFieldValue"
+          @click.stop="!loading ? updateFieldValue() : void 0"
         >
           <path
             d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-2.3-8.7 1.3 1.29 3.3-3.3a1 1 0 0 1 1.4 1.42l-4 4a1 1 0 0 1-1.4 0l-2-2a1 1 0 0 1 1.4-1.42z"
@@ -40,7 +41,7 @@
           width="24"
           height="24"
           class="inline-icon cancel-icon"
-          @click.capture="editing = false"
+          @click.stop="!loading ? (editing = false) : void 0"
         >
           <path
             d="M4.93 19.07A10 10 0 1 1 19.07 4.93 10 10 0 0 1 4.93 19.07zm1.41-1.41A8 8 0 1 0 17.66 6.34 8 8 0 0 0 6.34 17.66zM13.41 12l1.42 1.41a1 1 0 1 1-1.42 1.42L12 13.4l-1.41 1.42a1 1 0 1 1-1.42-1.42L10.6 12l-1.42-1.41a1 1 0 1 1 1.42-1.42L12 10.6l1.41-1.42a1 1 0 1 1 1.42 1.42L13.4 12z"
@@ -69,6 +70,11 @@ export default {
   methods: {
     onKeyPress(e) {
       if (e.which === 13) this.updateFieldValue();
+    },
+
+    startEditing() {
+      this.fieldValue = (this.field.value || '').trim();
+      this.editing = true;
     },
 
     async updateFieldValue() {
