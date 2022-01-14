4655bc0b3fc6968b53f227de9f7c94ba08b42c31
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2022-01-14 14:36:36 +0200

    Fix fields with no value not being editable on index view

--- resources/js/components/InlineTextEditableValue.vue
+++ resources/js/components/InlineTextEditableValue.vue
@@ -3,57 +3,56 @@
     :class="`nova-inline-text-field-index text-${field.textAlign}${editing ? ' -editing' : ''} w-full`"
     @dblclick.stop="startEditing"
   >
-    <template v-if="hasValue">
-      <template v-if="!editing">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          class="inline-icon edit-icon fill-current text-70"
-          @click.stop="startEditing"
-        >
-          <path
-            d="m6.3 12.3 10-10a1 1 0 0 1 1.4 0l4 4a1 1 0 0 1 0 1.4l-10 10a1 1 0 0 1-.7.3H7a1 1 0 0 1-1-1v-4a1 1 0 0 1 .3-.7zM8 16h2.59l9-9L17 4.41l-9 9V16zm10-2a1 1 0 0 1 2 0v6a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6c0-1.1.9-2 2-2h6a1 1 0 0 1 0 2H4v14h14v-6z"
-          />
-        </svg>
-        <div v-if="field.asHtml" v-html="field.value"></div>
-        <span v-else class="whitespace-no-wrap">{{ field.value }}</span>
-      </template>
-      <template v-else>
-        <input
-          ref="input"
-          v-model="fieldValue"
-          @keypress="onInputKeyPress"
-          type="text"
-          :disabled="loading"
-          class="form-control form-input w-full"
+    <template v-if="!editing">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        class="inline-icon edit-icon fill-current text-70"
+        @click.stop="startEditing"
+      >
+        <path
+          d="m6.3 12.3 10-10a1 1 0 0 1 1.4 0l4 4a1 1 0 0 1 0 1.4l-10 10a1 1 0 0 1-.7.3H7a1 1 0 0 1-1-1v-4a1 1 0 0 1 .3-.7zM8 16h2.59l9-9L17 4.41l-9 9V16zm10-2a1 1 0 0 1 2 0v6a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6c0-1.1.9-2 2-2h6a1 1 0 0 1 0 2H4v14h14v-6z"
         />
+      </svg>
 
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          class="inline-icon confirm-icon"
-          @click.stop="!loading ? updateFieldValue() : void 0"
-        >
-          <path
-            d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-2.3-8.7 1.3 1.29 3.3-3.3a1 1 0 0 1 1.4 1.42l-4 4a1 1 0 0 1-1.4 0l-2-2a1 1 0 0 1 1.4-1.42z"
-          />
-        </svg>
-
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          width="24"
-          height="24"
-          class="inline-icon cancel-icon"
-          @click.stop="cancelEditing"
-        >
-          <path
-            d="M4.93 19.07A10 10 0 1 1 19.07 4.93 10 10 0 0 1 4.93 19.07zm1.41-1.41A8 8 0 1 0 17.66 6.34 8 8 0 0 0 6.34 17.66zM13.41 12l1.42 1.41a1 1 0 1 1-1.42 1.42L12 13.4l-1.41 1.42a1 1 0 1 1-1.42-1.42L10.6 12l-1.42-1.41a1 1 0 1 1 1.42-1.42L12 10.6l1.41-1.42a1 1 0 1 1 1.42 1.42L13.4 12z"
-          />
-        </svg>
-      </template>
+      <div v-if="!hasValue"><p>&mdash;</p></div>
+      <div v-else-if="field.asHtml" v-html="field.value"></div>
+      <span v-else class="whitespace-no-wrap">{{ field.value }}</span>
     </template>
-    <p v-else>&mdash;</p>
+    <template v-else>
+      <input
+        ref="input"
+        v-model="fieldValue"
+        @keypress="onInputKeyPress"
+        type="text"
+        :disabled="loading"
+        class="form-control form-input w-full"
+      />
+
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        class="inline-icon confirm-icon"
+        @click.stop="!loading ? updateFieldValue() : void 0"
+      >
+        <path
+          d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-2.3-8.7 1.3 1.29 3.3-3.3a1 1 0 0 1 1.4 1.42l-4 4a1 1 0 0 1-1.4 0l-2-2a1 1 0 0 1 1.4-1.42z"
+        />
+      </svg>
+
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="24"
+        height="24"
+        class="inline-icon cancel-icon"
+        @click.stop="cancelEditing"
+      >
+        <path
+          d="M4.93 19.07A10 10 0 1 1 19.07 4.93 10 10 0 0 1 4.93 19.07zm1.41-1.41A8 8 0 1 0 17.66 6.34 8 8 0 0 0 6.34 17.66zM13.41 12l1.42 1.41a1 1 0 1 1-1.42 1.42L12 13.4l-1.41 1.42a1 1 0 1 1-1.42-1.42L10.6 12l-1.42-1.41a1 1 0 1 1 1.42-1.42L12 10.6l1.41-1.42a1 1 0 1 1 1.42 1.42L13.4 12z"
+        />
+      </svg>
+    </template>
   </div>
 </template>
 
